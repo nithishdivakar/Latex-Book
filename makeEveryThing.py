126549dadc97a34de3bd79ca5a611ff2c400e76d
nd <nd@gmail.com>
2015-10-09 18:55:36 +0530

    version 1

--- makeEveryThing.py
+++ makeEveryThing.py
@@ -5,10 +5,6 @@ with open("book/chapterlist","r") as f:
 		F=F.split()[0]
 		os.system("cd "+F+" && pdflatex "+F+".tex")
 		os.system("cd ../")
-with open("book/chapterlistadv","r") as f:
-	for F in f:
-		F=F.split()[0]
-		os.system("cd "+F+" && pdflatex "+F+".tex")
-		os.system("cd ../")
-os.system("cd book && pdflatex wot.tex")
+
+os.system("cd book && pdflatex latex-book.tex && pdflatex latex-book.tex")
 os.system("cd ../ && rm *.aux *.log")
