5f0ffb3bc347a67d252accb9ef0993d72413ac5b
nd <nd@gmail.com>
2015-10-20 22:56:30 +0530

    IMplemented individ images folder for chapters

--- makeChapter.py
+++ makeChapter.py
@@ -1,10 +1,27 @@
 import os
 import sys
 
+content='''
+\documentclass[../Lat-Book/Lat-Book.tex]{subfiles}
+\def\THIS{%s}
+
+\\begin{document}
+\INIT
+
+\chapter{%s}
+
+
+\end{document}
+'''
+
+
 if len(sys.argv)==3:
-    number = sys.argv[1]
-    name   = sys.argv[2]  
-    os.system("mkdir Chap%s-%s"%(number,name))
-    os.system("cp template.tex Chap%s-%s/Chap%s-%s.tex"%(number,name,number,name))
+	number = sys.argv[1]
+	name   = sys.argv[2]
+	q_path = "Chap%s-%s"%(number,name)
+	os.system("mkdir %s"%(q_path))
+	os.system("mkdir %s/images"%(q_path))
+	with open("%s/%s.tex"%(q_path,q_path),"w") as f:
+		f.write(content%(q_path,name))
 else:
-    print("makeChapter <number> <name>")
+	print("makeChapter <number> <name>")
--- makeEveryThing.py
+++ makeEveryThing.py
@@ -1,10 +1,10 @@
 import os
 
-with open("book/chapterlist","r") as f:
+with open("Lat-book/chapterlist","r") as f:
 	for F in f:
 		F=F.split()[0]
 		os.system("cd "+F+" && pdflatex "+F+".tex")
 		os.system("cd ../")
 
-os.system("cd book && pdflatex latex-book.tex && pdflatex latex-book.tex")
+os.system("cd Lat-book && pdflatex Lat-book.tex && pdflatex Lat-book.tex")
 os.system("cd ../ && rm *.aux *.log")
